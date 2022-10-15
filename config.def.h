b23e84726cee537ff0f41b9042cc61e50b603482
Vishal Balaji D <vishalbalaji@arch>
2022-10-15 19:14:44 +0530

    keybinds and stuff

--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int centerwindowname = 1;       /* 0 means window title is not centered */
 static const unsigned int maxtitlelength = 100;       /* max length for window title */
+/* static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10}; */
 static const unsigned int snap      = 32;       /* snap pixel */
 
 /* bar */
@@ -56,6 +57,7 @@ static const unsigned int alphas[][3]      = {
 	[SchemeInfoNorm] = { OPAQUE, baralpha, borderalpha },
 };
 
+/* autostart */
 static const char *const autostart[] = {
 	"dwm_time", NULL,
 	NULL /* terminate */
@@ -63,6 +65,17 @@ static const char *const autostart[] = {
 
 /* tagging */
 static const char *tags[] = { "1: ", "2: ", "3: ", "4: " };
+
+/* typedef struct { */
+/* 	const char *name; */
+/* 	const void *cmd; */
+/* } Sp; */
+/**/
+/* const char *spcmd1[] = {"kitty", "--title", "scratchpad", "--class", "scratchpad", "-o", "initial_window_width=800", "-o", "initial_window_height=530", "-o", "remember_window_size=no", NULL }; */
+/* static Sp scratchpads[] = { */
+/* 	/* name          cmd  */
+/* 	{"scratchpad",      spcmd1}, */
+/* }; */
 
 static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
@@ -118,47 +131,65 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-static const Key keys[] = {
+/* #include "movestack.c" */
+static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	/* { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } }, */
+	/* { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } }, */
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,                       XK_i,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_5,      setmfact,       {.f = +1.55} },
+	{ MODKEY,                       XK_6,      setmfact,       {.f = +1.65} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+  { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	/* { MODKEY,                       XK_s,      togglesticky,   {0} }, */
+	/*  { MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} }, */
+	/*  { MODKEY|ShiftMask,             XK_s,      togglealwaysontop, {0} }, */
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	/* { MODKEY,                       XK_x,      movecenter,     {0} }, */
+	/* { MODKEY,                             XK_grave,  togglescratch,  {.ui = 0 } }, */
+	/* { MODKEY,                             XK_7,      setborderpx,    {.i = -1 } }, */
+	/* { MODKEY,                             XK_8,      setborderpx,    {.i = +1 } }, */
+	/* { MODKEY,                             XK_9,      setborderpx,    {.i = 0 } }, */
+	/* { MODKEY,                       XK_minus,  setgaps,        {.i = -5 } }, */
+	/* { MODKEY,                       XK_equal,  setgaps,        {.i = +5 } }, */
+	/* { MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } }, */
+	/* { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} }, */
+  { MODKEY|ShiftMask,             XK_r,      quit,           {1} },
 	TAGKEYS(                        XK_1,                      0)
+  TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	{ MODKEY|ShiftMask, XK_r,      quit,           {1} }, 
 };
+
+static const char *statuscmd[] = { "sh", "-c", "notify-send -h string:x-canonical-private-synchronous:date \"  $(date '+%A, %d %B %Y')\"", NULL };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static const Button buttons[] = {
+static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button1,        spawn,          {.v = statuscmd } },
+	{ ClkStatusText,        0,              Button3,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
