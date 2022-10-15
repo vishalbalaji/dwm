97b042fe926a5ecf2e3373f43b46ac1fc285b624
Vishal Balaji D <vishalbalaji@arch>
2022-10-15 18:46:26 +0530

    barheight

--- dwm.c
+++ dwm.c
@@ -1791,7 +1791,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
-	bh = drw->fonts->h + 2;
+	bh = user_bh ? user_bh : drw->fonts->h + 2;
 	sp = sidepad;
 	vp = (topbar == 1) ? vertpad : - vertpad;
 	updategeom();
@@ -2341,7 +2341,7 @@ updatesystray(int updatebar)
 	#endif // BARPADDING_PATCH
 
   unsigned int x = m->mx + m->mw - sp;
-  unsigned int y = m->by + vp - 1;
+  unsigned int y = m->by + vp - 0.5;
 
 	if (!showsystray)
 		return;
